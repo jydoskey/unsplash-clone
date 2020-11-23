0989449ccdd76b35e6c2086467d2ea81bd24c33e
jide_omotola <jide@reach.africa>
2020-11-23 22:56:36 +0100

    added themed bootstrap link and pptys for project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div id="app">
-    <Landing/>
+    <Landing />
   </div>
 </template>
 
 <script>
-import Landing from './components/Landing.vue'
+  import Landing from './components/Landing.vue'
 
-export default {
-  name: 'App',
-  components: {
-    Landing
+  export default {
+    name: 'App',
+    components: {
+      Landing
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import "@/scss/themed-bootstrap.scss";
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+  }
+
+  html {
+    font-size: 14px;
+  }
 </style>
